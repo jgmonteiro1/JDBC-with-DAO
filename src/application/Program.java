cdb629f9e09e3b94413c728faef46602f2bc2064
João Gabriel <jgmoreira15@gmail.com>
2019-09-03 22:56:23 -0300

    Criação da classe vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "books");
+		Seller seller = new Seller(21, "João", "jgmoreira15@gmail.com", new Date(), 3000.0, obj);
 		
-		System.out.println(obj);
+		System.out.println(seller);
 	}
 }
