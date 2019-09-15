8e38c6dae38acf93c821bd456b803ad1baab8c1b
João Gabriel <jgmoreira15@gmail.com>
2019-09-14 21:18:59 -0300

    Correção da implementação errada da classe DepartmentDao

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -2,17 +2,17 @@ package model.dao.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
 import db.DB;
 import db.DbException;
-import model.dao.SellerDao;
+import model.dao.DepartmentDao;
 import model.entities.Department;
-import model.entities.Seller;
 
-public class DepartmentDaoJDBC implements SellerDao {
+public class DepartmentDaoJDBC implements DepartmentDao {
 
 	private Connection conn;
 	
@@ -22,7 +22,7 @@ public class DepartmentDaoJDBC implements SellerDao {
 	}
 
 	@Override
-	public void insert(Seller obj) {
+	public void insert(Department obj) {
 		PreparedStatement st = null;
 		try {
 		 st = conn.prepareStatement("INSERT INTO department"
@@ -42,7 +42,7 @@ public class DepartmentDaoJDBC implements SellerDao {
 	}
 
 	@Override
-	public void update(Seller obj) {
+	public void update(Department obj) {
 		PreparedStatement st = null;
 		try {
 			st = conn.prepareStatement("UPDATE department" + 
@@ -74,21 +74,37 @@ public class DepartmentDaoJDBC implements SellerDao {
 	}
 
 	@Override
-	public Seller findById(Integer id) {
+	public Department findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT * FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			if(rs.next()) {
+				Department obj = new Department();
+				obj.setId(id);
+				obj.setName(rs.getString("Name"));
+				return obj;
+			}
+			
+			return null;
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
+	}
+
+	@Override
+	public List<Department> findAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public List<Seller> findByDepartment(Department department) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
